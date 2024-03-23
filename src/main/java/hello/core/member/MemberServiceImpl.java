47c0f4e2463ffdda5736b52535d75bdd3def1377
Hanbit <ghiesf@naver.com>
2024-03-23 11:46:22 +0900

    Refactor: 객체지향 원리 적용

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
     // 인터페이스만 가지고 있다면 npe 발생할 것 -> 구현 객체 선택 필요
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
